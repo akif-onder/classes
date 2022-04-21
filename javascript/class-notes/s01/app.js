7180f40678a75eb79c1c740c683450ec1a205489
Matthew D <67597416+cwmatthew@users.noreply.github.com>
2022-04-21 23:03:27 +0300

    intro

---
+++ javascript/class-notes/s01/app.js
@@ -0,0 +1,10 @@
+console.log("app.js is running");
+
+let ad = prompt("Adinizi girin");
+
+// let sonuc = confirm("Emin misiniz");
+
+// console.log(sonuc);
+
+console.log("adiniz : " + ad + " dir");
+console.log(`adiniz : ${ad} dir`);
