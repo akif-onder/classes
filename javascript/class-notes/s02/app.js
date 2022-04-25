6f864948cbab7defcc7fcfefbd7ba1b6e124973b
Mark-Mad <mark.m@clarusway.com>
2022-04-25 22:15:53 +0300

    1. ders sonu

--- javascript/class-notes/s02/app.js
+++ javascript/class-notes/s02/app.js
@@ -1,1 +1,41 @@
+// let a;
+// console.log(a);
+// console.log(b);
+// console.log('hello')
 
+
+// console.log(a);
+//  var a = 3;
+//  var aa = 2;
+//  console.log(a);
+
+// console.log(b);
+// let b = 1;12
+// console.log(b);
+
+
+// let c = '2';
+// let d = 2;
+
+// console.log(typeof c)
+// console.log(typeof d)
+
+// let e = +prompt('Bir şey yazınız : ');
+
+// console.log(typeof e);
+
+
+// let f = null;
+// console.log(typeof f)
+
+
+// let g = 3.15;
+// let h = 1_000_000;
+
+// console.log(typeof g)
+// console.log(typeof h)
+
+let i = 999_999_999_999_999;
+console.log(i)
+let j = 999_999_999_999_868_123;
+console.log(j)
