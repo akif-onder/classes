a54eed2cf7089df5019bcb810e7c3812ee6ea1e2
profnoah <noah@clarusway.com>
2022-05-10 21:33:05 +0300

    not giriş

--- javascript/class-notes/js04_loops/loops.js
+++ javascript/class-notes/js04_loops/loops.js
@@ -10,31 +10,49 @@ console.log('****** LOOPS *******');
 // Math.round() => değerine göre yuvarlar.
 // Math.trunc() => kesirli sayının tam kısmı alır.
 
-for (let i = 1; i <= 10; i++) {
-  const randomSayi = Math.floor(Math.random() * 100 + 1);
-  console.log(randomSayi);
-}
+// for (let i = 1; i <= 10; i++) {
+//   const randomSayi = Math.floor(Math.random() * 100 + 1);
+//   console.log(randomSayi);
+// }
 
-console.log('Program bitti');
+// console.log('Program bitti');
 
 //? Ornek: 1 den n kadar sayıları toplayan kodu yazınız.
 
-const n = prompt('n sayisini girinz:');
-let toplam = 0;
-for (let i = 1; i <= n; i++) {
-  toplam += i;
-}
+// const n = prompt('n sayisini girinz:');
+// let toplam = 0;
+// for (let i = 1; i <= n; i++) {
+//   toplam += i;
+// }
 
-console.log('Toplam:', toplam);
+// console.log('Toplam:', toplam);
 
 //? ÖRNEK: Dışarıdan girilen sayının Asal olup olmadığını tespit ederek sonucu yazdıran programı yazınız.
 
 //******************** WHILE ******************
 
-let i = 1;
-while (i <= 5) {
-  console.log('Merhaba-', i);
-  i++;
-}
+// let i = 1;
+// while (i <= 5) {
+//   console.log('Merhaba-', i);
+//   i++;
+// }
 
-console.log('Bitti');
+// console.log('Bitti');
+
+// let not = prompt('Lütfen notunuzu giriniz (0-100):');
+
+// while (not < 0 || not > 100) {
+//   alert('Girdiğiniz not 0-100 arasında olmalıdır');
+//   not = prompt('Lütfen notunuzu giriniz (0-100):');
+// }
+// console.log('Girdiğiniz not', not);
+
+//* Ornek
+
+//******************** DO-WHILE ******************
+// let i = 1;
+
+// do {
+//   console.log('Merhaba-', i);
+//   i++;
+// } while (i <= 5);
