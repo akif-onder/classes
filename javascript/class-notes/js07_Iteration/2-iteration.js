f537f7a32d1e87034c1c157783ea8ef90579a495
profnoah <noah@clarusway.com>
2022-05-23 21:04:43 +0300

    iteration started

---
+++ javascript/class-notes/js07_Iteration/2-iteration.js
@@ -0,0 +1,8 @@
+//* ======================================================
+//*                   FOREACH METHOD
+//* ======================================================
+
+console.log('****** ITERATION **********');
+//*Dizideki herbir fiyati konsola bastiriniz.
+
+const fiyatlar = [100, 250, 50, 89];
