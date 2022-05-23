aee3b0ff5b66a7ebc48bf76b692913618418afcd
profnoah <noah@clarusway.com>
2022-05-23 22:01:27 +0300

    foreach

--- javascript/class-notes/js07_Iteration/2-iteration.js
+++ javascript/class-notes/js07_Iteration/2-iteration.js
@@ -6,3 +6,44 @@ console.log('****** ITERATION **********');
 //*Dizideki herbir fiyati konsola bastiriniz.
 
 const fiyatlar = [100, 250, 50, 89];
+
+//*Dizideki herbir fiyati konsola bastiriniz.
+
+fiyatlar.forEach(yazdir);
+
+function yazdir(v) {
+  console.log(v);
+}
+
+console.log('***************************');
+
+//! Arraw function kullanılarak çözüm
+fiyatlar.forEach((value) => console.log(value));
+
+//** Fiyatlar dizisindekilerin toplamini konsola yazdiriniz */
+let toplam = 0;
+fiyatlar.forEach((fiyat) => (toplam += fiyat));
+console.log('Toplam:', toplam);
+
+//! NOT: forEach metodu void bir metottur.
+//! (Yani herhangi bir değer döndürmez.)
+console.log(fiyatlar.forEach((fiyat) => (toplam += fiyat))); //!undefined
+
+//*-------------- ÖRNEK -------------------
+
+let toplam1 = 0;
+
+fiyatlar.forEach((deger, indis, dizi) => {
+  toplam1 += deger;
+  console.log(`${indis}.iterasyon toplami:${toplam1}`);
+  dizi[indis] = deger + 10;
+});
+console.log(fiyatlar);
+
+//*-------------- ÖDEV -------------------
+//* fiyatlar dizisinde her bir fiyata %10 zam yapalım.
+
+
+//* ======================================================
+//*                       MAP METHOD
+//* ======================================================
