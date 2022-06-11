4e625101d2c2af19397d7e7ec518268964974a51
profnoah <noah@clarusway.com>
2022-06-11 12:00:09 +0300

    append

--- javascript/class-notes/js13_ProjectWeek_IOSCalc/app.js
+++ javascript/class-notes/js13_ProjectWeek_IOSCalc/app.js
@@ -6,12 +6,25 @@ const currDisp = document.querySelector('.current-display');
 
 const btnContainer = document.querySelector('.buttons-container');
 
+let currOperand = '';
+let previousOperand = '';
+
 //? Butonlari tasiyan container icin event tanimlamasi
 btnContainer.addEventListener('click', (e) => {
   if (e.target.classList.contains('num')) {
-    // console.log(e.target.textContent);
     appendNumber(e.target.textContent);
+    updateDisplay();
   }
 });
 
-const appendNumber = () => {};
+const appendNumber = (num) => {
+  if (num === '.' && String(currOperand).includes('.')) return;
+
+  if (!currOperand && num === '0') return;
+
+  currOperand = num;
+};
+
+const updateDisplay = () => {
+  currDisp.textContent += currOperand;
+};
