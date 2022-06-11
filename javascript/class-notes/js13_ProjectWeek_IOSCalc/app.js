86ee6b9b8c5e15c099dfd92ca5dda6131fe732c7
profnoah <noah@clarusway.com>
2022-06-11 11:35:28 +0300

    append

--- javascript/class-notes/js13_ProjectWeek_IOSCalc/app.js
+++ javascript/class-notes/js13_ProjectWeek_IOSCalc/app.js
@@ -1,3 +1,17 @@
-//* ======================================================
+//* =================================================
 //*                     IOS CALCULATOR
-//* ======================================================
+//* =================================================
+const prevDisp = document.querySelector('.previous-display');
+const currDisp = document.querySelector('.current-display');
+
+const btnContainer = document.querySelector('.buttons-container');
+
+//? Butonlari tasiyan container icin event tanimlamasi
+btnContainer.addEventListener('click', (e) => {
+  if (e.target.classList.contains('num')) {
+    // console.log(e.target.textContent);
+    appendNumber(e.target.textContent);
+  }
+});
+
+const appendNumber = () => {};
