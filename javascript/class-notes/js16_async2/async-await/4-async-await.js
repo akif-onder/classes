be3a29c0b22ee0040d79ac1d1e1c1c688d3edd35
profnoah <noah@clarusway.com>
2022-06-15 21:02:06 +0300

    async

--- javascript/class-notes/js16_async2/async-await/4-async-await.js
+++ javascript/class-notes/js16_async2/async-await/4-async-await.js
@@ -6,7 +6,7 @@
 //? Bu baglamda sentetik seker benzetmesi yapilabilir.
 
 //* Bir fonskiyonu asyn hale getirmek icin fonksiyon keyword'nun onune
-//* asyn keyword'u eklenir.
+//* async keyword'u eklenir.
 
 //* Bir async fonksiyon icerisinde await keyword'u ile yapilan istegin cevabinin
 //* beklenmesi saglanir.
@@ -17,3 +17,37 @@
 //* Await, promise-temelli herhangi bir fonksiyonun onune getirilerek getirildigi
 //* satirdaki kodun durudurulmasini saglar. Yapilan istek yerine getirilip sonuc
 //* degerlerinin dondurulmesine ile kodun calismasi devam eder.
+
+let error = false;
+const getUsers = async function () {
+  try {
+    const res = await fetch('https://api.github.com/user');
+    if (!res.ok) {
+      error = true;
+      throw new Error(`Something went wrong:${res.status}`);
+    }
+    const data = await res.json();
+    updateDom(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getUsers();
+
+const updateDom = (data) => {
+  const userDiv = document.querySelector('.users');
+  if (error) {
+    userDiv.innerHTML = `<h1 class="text-danger">Data can not be fetched</h1>`;
+  } else {
+    data.forEach((user) => {
+      //!destr
+      const { login, avatar_url, html_url } = user;
+      userDiv.innerHTML += `
+    <h2 class="text-warning">NAME:${login}</h2>
+    <img src=${avatar_url} width="50%" alt="" />
+    <h3>HTML_URL:${html_url}</h3>
+  `;
+    });
+  }
+};
