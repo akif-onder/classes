cb7d4c5bdb2945c62728e7e457b32a01076c34d0
profnoah <noah@clarusway.com>
2022-06-22 20:31:28 +0300

    app

--- react/class-notes/rc02_jsx_basicStyling/src/App.js
+++ react/class-notes/rc02_jsx_basicStyling/src/App.js
@@ -1,3 +1,17 @@
+//? React,JSX kullanmaktadir.
+//? JSX'de, HTMl elementleri dogrudan JS icerisinde kullanilabilir
+
+//! Componentler, HTML,CSS ve JS kodlarindan meydana gelmiş
+//! bir kod parcacigidir.
+
+//! Bir componentin return () kismi ise JSX kodlari icerir.
+
+//! JSX'in kendine gore bazi kurallari vardir. Ornegin bir
+//! component eger birden fazla html elementi dondurmesi
+//! gerekirse bunlari tek bir container elemani ile sarmayalarak
+//! dondurmelidir.container icin div, article, header, <> v.b
+//! herhangi bir element kullanilabilir.
+
 //? App componentinin tanimlanmasi
 function App() {
   return (
