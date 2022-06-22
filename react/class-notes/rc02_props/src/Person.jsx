841cd065d86c24d0cee2e47f33a7ce3b0c6ad66c
profnoah <noah@clarusway.com>
2022-06-22 22:50:40 +0300

    props

--- react/class-notes/rc02_props/src/Person.jsx
+++ react/class-notes/rc02_props/src/Person.jsx
@@ -1,5 +1,14 @@
-const Person = () => {
-  return <div>Person</div>;
+import Msg from './Msg';
+
+const Person = ({ name, img, tel }) => {
+  // const { name, img, tel } = props;
+  return (
+    <div>
+      <Msg  name={name}/>
+      <img src={img} alt="" />
+      <p>{tel}</p>
+    </div>
+  );
 };
 
 export default Person;
