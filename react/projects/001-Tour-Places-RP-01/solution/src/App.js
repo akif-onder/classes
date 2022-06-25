d08331912967a69611da5725129684452fdaa910
HarveyCla <harvey@clarusway.com>
2022-06-25 11:42:38 +0300

    navbar

--- react/projects/001-Tour-Places-RP-01/solution/src/App.js
+++ react/projects/001-Tour-Places-RP-01/solution/src/App.js
@@ -1,10 +1,10 @@
 import './App.scss'
+import Navbar from './components/Navbar/Navbar';
 
 const App = () =>{
   return(
     <div className="App">
-      <h1>Welcome react</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis impedit, iure provident sint ex vero animi sapiente consectetur error pariatur amet beatae nemo illum, nobis alias aspernatur molestias dignissimos harum!</p>
+      <Navbar/>
     </div>
   )
 }
