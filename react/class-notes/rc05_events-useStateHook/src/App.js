0038843b6d3222d7ae43558782dbf238d3adeab2
profnoah <noah@clarusway.com>
2022-06-27 22:08:06 +0300

    USeState

--- react/class-notes/rc05_events-useStateHook/src/App.js
+++ react/class-notes/rc05_events-useStateHook/src/App.js
@@ -1,5 +1,6 @@
 import Events from './components/1-event-intro/Events';
 import Counter from './components/2-classComponents/Counter';
+import UseStateExample from './components/3-useStateHook/UseStateExample';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Events />
       <Counter count={10}/>
       <Counter />
+      <UseStateExample/>
     </div>
   );
 }
