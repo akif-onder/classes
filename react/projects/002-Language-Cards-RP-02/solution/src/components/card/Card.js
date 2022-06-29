50ed1758775c8f8e9e8ca4465721fb34feef3335
8490 <ethan@clarusway.com>
2022-06-29 21:27:35 +0300

    ıtem js

--- react/projects/002-Language-Cards-RP-02/solution/src/components/card/Card.js
+++ react/projects/002-Language-Cards-RP-02/solution/src/components/card/Card.js
@@ -1,4 +1,5 @@
 import { languages } from '../../helpers/data';
+import Item from '../item/Item';
 
 const Card = () => {
   return (
@@ -7,7 +8,7 @@ const Card = () => {
       <h1 className="language-title">Languages</h1>
       <div className="cards-container">
         {languages.map((item, index) => {
-          // return <Item card={item} />;
+          return <Item card={item} />;
         })}
       </div>
     </div>
